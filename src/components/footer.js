d5db982abed8e5d378bd291e0522601517cc0d54
wrewsama <andrewlzs512@gmail.com>
2023-01-06 17:53:10 +0800

    add footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function MainFooter() {
     return (
-        <div>Footer</div>
+        <footer className="py-3 mt-auto bg-dark">
+            <div className="container px-4">
+                <p className="m-0 text-center text-white">
+                    &copy; Copyright 2023. Website by&nbsp;
+                    <a className="text-decoration-none text-light font-weight-bold"
+                       href="https://github.com/wrewsama">
+                        wrewsama
+                    </a>
+                </p>
+            </div>
+      </footer>
     )
 }
