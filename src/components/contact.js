314315654cc185a952c9bc78c020fe318fefd564
wrewsama <andrewlzs512@gmail.com>
2023-01-06 17:53:03 +0800

    add contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import githubIcon from '../img/github.png'
+import emailIcon from "../img/email.png"
+import linkedinIcon from "../img/linkedin.png"
+
 
 export default function Contact() {
     return (
-        <div>Contact</div>
+        <div className="row bg-light">
+            <div className="text-center">
+                <h2>Contact</h2>
+            </div>
+
+            <div className="col d-flex justify-content-center">
+                <a href="https://github.com/wrewsama">
+                    <img src={githubIcon} className="rounded" alt="github icon" />
+                </a>
+            </div>
+
+            <div className="col d-flex justify-content-center">
+                <a href="mailto:andrewlzs512@gmail.com">
+                    <img src={emailIcon} class="rounded" alt="email icon" />
+                </a>
+            </div>
+
+            <div className="col d-flex justify-content-center">
+                <a href="https://www.linkedin.com/in/andrewlozhisheng/">
+                    <img src={linkedinIcon} class="rounded" alt="linkedin icon" />
+                </a>
+            </div>
+            
+        </div>
     )
 }
