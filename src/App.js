6f1ff21dbf48350a30f0da53e225023d773d1242
wrewsama <andrewlzs512@gmail.com>
2023-01-06 15:57:04 +0800

    create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Container from 'react-bootstrap/Container'
+import About from './components/about';
+import Contact from './components/contact';
+import Experience from './components/experience';
+import MainFooter from './components/footer';
+import Home from './components/home';
+import MainNavbar from './components/navbar';
+import Projects from './components/projects';
 
 function App() {
 	return (
 		<Container>
-			
+			<MainNavbar />
+			<Home />
+			<About />
+			<Experience />
+			<Projects />
+			<Contact />
+			<MainFooter />
 		</Container>
 	);
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MainNavbar() {
+    return (
+        <div>Navbar</div>
+    )
+}
