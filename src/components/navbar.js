ded6d46507394c825b8402bb91c1fc787fd2f192
wrewsama <andrewlzs512@gmail.com>
2023-01-09 21:19:54 +0800

    add navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function MainNavbar() {
     return (
-        <div>Navbar</div>
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <div className="container-fluid">
+                <span className="navbar-brand text-white">wrewsama</span>
+
+                
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link text-white" href="#home">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-white" href="#about">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-white" href="#experience">Experience</a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a className="nav-link text-white" href="#projects">Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-white" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
