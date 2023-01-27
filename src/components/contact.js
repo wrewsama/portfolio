689be6e544f512de5f709a2c3c978ebd37572588
wrewsama <andrewlzs512@gmail.com>
2023-01-27 21:22:48 +0800

    update contact icon borders

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,25 +7,25 @@ import linkedinIcon from "../img/linkedin.png"
 export default function Contact() {
     return (
         <div className="row my-5" data-aos="fade-up" id="contact">
-            <div className="text-center text-heading">
+            <div className="text-center text-heading mb-5">
                 <h2>Contact</h2>
             </div>
 
             <div className="col d-flex justify-content-center">
                 <a href="https://github.com/wrewsama">
-                    <img src={githubIcon} className="rounded" alt="github icon" />
+                    <img src={githubIcon} className="rounded-circle border" alt="github icon" />
                 </a>
             </div>
 
             <div className="col d-flex justify-content-center">
                 <a href="mailto:andrewlzs512@gmail.com">
-                    <img src={emailIcon} className="rounded" alt="email icon" />
+                    <img src={emailIcon} className="rounded-circle border" alt="email icon" />
                 </a>
             </div>
 
             <div className="col d-flex justify-content-center">
                 <a href="https://www.linkedin.com/in/andrewlozhisheng/">
-                    <img src={linkedinIcon} className="rounded" alt="linkedin icon" />
+                    <img src={linkedinIcon} className="rounded-circle border" alt="linkedin icon" />
                 </a>
             </div>
             
