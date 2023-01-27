b295646f21a28ee19ec0216b36ba77f95f296ef0
wrewsama <andrewlzs512@gmail.com>
2023-01-27 21:38:54 +0800

    implement collapsible navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'
 import { useEffect } from 'react';
 import About from './components/about';
 import Contact from './components/contact';
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,25 +6,31 @@ export default function MainNavbar() {
             <div className="container-fluid">
                 <a className="navbar-brand text-heading" href="#home">wrewsama</a>
 
-                
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link text-subheading" href="#home">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-subheading" href="#about">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-subheading" href="#experience">Experience</a>
-                    </li>
+                <button className="navbar-toggler light-dropdown-btn"
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarSupportedContent'>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link text-subheading" href="#home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-subheading" href="#about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-subheading" href="#experience">Experience</a>
+                        </li>
 
-                    <li className="nav-item">
-                        <a className="nav-link text-subheading" href="#projects">Projects</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-subheading" href="#contact">Contact</a>
-                    </li>
-                </ul>
+                        <li className="nav-item">
+                            <a className="nav-link text-subheading" href="#projects">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-subheading" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
