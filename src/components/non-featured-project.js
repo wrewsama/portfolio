749c3d58b625b9dfbd0c51f899f0cf48f3848847
wrewsama <andrewlzs512@gmail.com>
2023-03-05 10:33:41 +0800

    add hover effects for non featured project cards

--- src/components/non-featured-project.js
+++ src/components/non-featured-project.js
@@ -3,13 +3,14 @@ import React from 'react'
 export default function NonFeaturedProject({ img, title, desc, link }) {
     return (
         <div className="col-4 d-flex justify-content-center">
-            <div className="card text-center" style={{height: '15rem', width: '15rem'}}>
+            <div className="card bg-dark text-center" style={{height: '15rem', width: '15rem'}}>
                 <img src={img} className="card-img" style={{height: '15rem', width: '15rem'}} />
+
                 <div className="card-img-overlay">
-                    <h5 className='card-title'>{title}</h5>
-                    <p className="card-text">{desc}</p>
+                    <h4 className='card-title text-heading'>{title}</h4>
+                    <p className="card-text text-normal">{desc}</p>
                     <a href={link.url}>
-                        <button className='btn'>{link.name}</button>
+                        <button className='btn highlighted-btn'>{link.name}</button>
                     </a>
                 </div>
             </div>
--- src/components/other-projects.js
+++ src/components/other-projects.js
@@ -4,12 +4,30 @@ import hnrIcon from '../img/hnr.png'
 
 export default function OtherProjects() {
     return (
-        <div className="container" id="other-projects" data-aos="fade-up">
-            <div className="row text-center text-heading">
+        <div className="container my-5" id="other-projects" data-aos="fade-up">
+            <div className="row text-center text-heading mb-5">
                 <h2>Other Projects</h2>
             </div>
 
-            <div className="row">
+            <div className="row d-flex justify-content-center">
+                <NonFeaturedProject title="Hydration Tracker"
+                                    img={hnrIcon}
+                                    desc="A web app to track water intake. Made with MongoDB, Express, React, and Node."
+                                    link={{
+                                        name: "website",
+                                        url: "https://hnr2023-stayhydrated.onrender.com/"
+                                    }}
+                                     />
+
+                <NonFeaturedProject title="Hydration Tracker"
+                                    img={hnrIcon}
+                                    desc="A web app to track water intake."
+                                    link={{
+                                        name: "website",
+                                        url: "https://hnr2023-stayhydrated.onrender.com/"
+                                    }}
+                                     />
+                
                 <NonFeaturedProject title="Hydration Tracker"
                                     img={hnrIcon}
                                     desc="A web app to track water intake."
